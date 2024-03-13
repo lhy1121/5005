4dd888b9f6ae0ca2d75c5bf9714be1f22c71f68b
HAOYANG LU <john101112@163.com>
2024-03-13 12:32:31 +0000

    getdata

---
+++ midterm/f1data/get_data.py
@@ -0,0 +1,11 @@
+import fastf1
+import csv
+
+session = fastf1.get_session(2024, '2024 Bahrain Race', 'Q')
+session.load()
+data = session.laps
+alb = data[data['DriverNumber'] == '23']
+sar = data[data['DriverNumber'] == '2']
+alb.to_csv("ALB_R.csv",columns = alb.columns,index = True,header = True)
+sar.to_csv("SAR_R.csv",columns = sar.columns,index = True,header = True)
+
